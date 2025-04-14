58fff352c8dae2d38650abdbe0dc942a8fec5fc8
TheDrakl <denysmelnyk262626@gmail.com>
2025-04-14 13:18:01 +0100

    Fixed users model

--- users/models.py
+++ users/models.py
@@ -26,6 +26,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
     
     def __str__(self):
         return self.email
