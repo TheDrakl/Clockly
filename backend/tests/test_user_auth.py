af197fd3d28a6cd6fd5a5a6b2c14c655a3db51de
TheDrakl <denysmelnyk262626@gmail.com>
2025-05-12 19:19:42 +0100

    Updated tests

--- backend/tests/test_user_auth.py
+++ backend/tests/test_user_auth.py
@@ -32,10 +32,7 @@ class UserTests(APITestCase):
             "password2": self.password,
             "verification_code": code
         }
-        verify_response = self.client.post(verify_url, verify_data, format='json')
-
-        self.refresh_token = verify_response.data['refresh_token']
-        self.access_token = verify_response.data['access_token']
+        self.verify_response = self.client.post(verify_url, verify_data, format='json')
 
         # Login
         login_url = reverse('login')
@@ -51,26 +48,33 @@ class UserTests(APITestCase):
     def test_verification_code(self):
         User = get_user_model()
         self.assertTrue(User.objects.filter(email=self.email).exists())
-        
-        self.assertIsNotNone(self.access_token)
-        self.assertIsNotNone(self.refresh_token)
+        # Check cookies set
+        self.assertIn('access_token', self.verify_response.cookies)
+        self.assertIn('refresh_token', self.verify_response.cookies)
 
     def test_login(self):
         login_response = self.login_response
         self.assertEqual(login_response.status_code, status.HTTP_200_OK)
-        self.assertIn('access_token', login_response.data)
-        self.assertIn('refresh_token', login_response.data)
+        # Check cookies set
+        self.assertIn('access_token', login_response.cookies)
+        self.assertIn('refresh_token', login_response.cookies)
 
+    def test_authenticated_request(self):
+        # Simulate logged-in user by setting the access_token cookie
+        access_token = self.login_response.cookies['access_token'].value
+        self.client.cookies['access_token'] = access_token
+        url = reverse('check-auth')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.data.get('is_authenticated'))
 
     def test_refresh_token(self):
+        # Set refresh_token cookie
+        refresh_token = self.login_response.cookies['refresh_token'].value
+        self.client.cookies['refresh_token'] = refresh_token
         url = reverse('token-refresh')
-        data = {
-            "refresh": self.refresh_token
-        }
-        response = self.client.post(url, data, format='json')
-
+        response = self.client.post(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('access', response.data)
-        self.assertIn('refresh', response.data)
-
-    
+        # Should set new access_token and refresh_token cookies
+        self.assertIn('access_token', response.cookies)
+        self.assertIn('refresh_token', response.cookies)
